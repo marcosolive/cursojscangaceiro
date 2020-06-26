939e34696be65317fd43cb0e02352cf7c9c1643b
Marcos Oliveira <marcos.oliveira@bairral.local>
2020-06-26 16:02:20 -0300

    Atualizacao constructor negociacao

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -1,8 +1,9 @@
 class Negociacao {
     constructor(data, quantidade, valor) {
-        this._data = data;
+        this._data =  new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
+        Object.freeze(this);
     }
 
     getVolume() {
@@ -10,7 +11,7 @@ class Negociacao {
     }
 
     getData(){
-        return this._data;
+        return new Date (this._data.getTime());
     }
 
     getQuantidade(){
